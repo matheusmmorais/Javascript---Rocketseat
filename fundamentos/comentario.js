4b5866b798c4c6bee605231272915f08fc472d61
matheusmmorais <matheus_mmorais@hotmail.com>
2022-02-22 14:15:07 -0300

    update

---
+++ fundamentos/comentario.js
@@ -0,0 +1,7 @@
+// aqui eu tenho um comentario em linha
+
+/* aqui eu tenho um comentario 
+multi linha*/
+
+console.log('bem vindo ao starter');
+console.log('nao iria ignorar');
---
+++ tiposdedados.js
@@ -0,0 +1,46 @@
+/* 
+    Tipo de dados:
+
+    Conforme o ECMAscript standard temos 9 tipos de dados:
+
+    ##Data Types
+    * primitive
+    * structural
+    * structural primitive
+    
+    ##Primitivos
+    * String
+    * Number
+    * Boolean
+    * Undefined
+    * Symbol
+    * BigInt
+    
+    #Estruturais
+    *Object
+        * Array
+        * Map
+        * Set
+        * Date
+    *Function
+
+    ##Primitivo Estrutural
+    *Null
+*/
+
+
+
+
+console.log(`nome ${1+1}`);
+console.log("12.5 + 12");
+
+
+Object ({
+    nome: "matheus",
+    idade: "25",
+    andar: function(){
+        console.log("teste");
+    }
+})
+
+console.log(['leite','ovos', 1, 2, 3]);

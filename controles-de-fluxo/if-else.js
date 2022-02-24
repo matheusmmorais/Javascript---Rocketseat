6ab48675945f2f494dc3c5ed9d4733a954f5bff5
matheusmmorais <matheus_mmorais@hotmail.com>
2022-02-24 11:25:40 -0300

    Create if-else.js

---
+++ controles-de-fluxo/if-else.js
@@ -0,0 +1,9 @@
+let temperatura = 37.5;
+
+if (temperatura >= 38){
+    console.log("está com febre alta");
+} else if (temperatura <= 38 && temperatura >= 37) {
+    console.log("está com febre mediana");
+} else {
+    console.log("saudável");
+}

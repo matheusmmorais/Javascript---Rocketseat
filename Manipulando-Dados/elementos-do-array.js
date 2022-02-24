bfe30bf578baed28b5e2908eb28d323aa1be620c
matheusmmorais <matheus_mmorais@hotmail.com>
2022-02-23 23:42:42 -0300

    updates

---
+++ Manipulando-Dados/elementos-do-array.js
@@ -0,0 +1,6 @@
+// manipulando arrays
+
+//contar elementos de um array.
+
+let array = ['a','b','c'];
+console.log(array.length[2]);
---
+++ Manipulando-Dados/manipulando-arrays.js
@@ -0,0 +1,16 @@
+// manipulando arrays
+
+let techs = ['html','css','js'];
+
+// adicionar um item no fim
+console.log(techs.push("nodejs"));
+// adicionar no começo
+console.log(techs.unshift("sql"));
+// remover do fim
+console.log(techs.pop());
+// remover do começo
+console.log(techs.shift());
+// pegar somente alguns elementos do array
+console.log(techs.slice(1,3));
+// remover 1 ou mais itens em qualquer posicao do array
+// encontrar aposicao de um elemento no array
